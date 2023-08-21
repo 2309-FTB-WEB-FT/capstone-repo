f305638409b7eb5cb1cfd52a0198ea08064823b5
RaccoonButler <raccoonutau@gmail.com>
2023-08-21 10:52:57 -0500

    Set up react router navbar

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,27 +1,27 @@
-import { BrowserRouter as Routes, Route, } from 'react-router-dom';
-import React, { useState } from 'react'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Shows from './components/Shows';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
         <Navbar />
         <h1>Boilerplate</h1>
-        <img id='comp-img' src='./computer.png'></img>
+        <img id='comp-img' src='./computer.png' alt="computer" />
         <p>Replace the starter code in this template with something cool</p>
         <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/Shows" component={Shows} />
-          <Route path="/Login" component={Login} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Shows" element={<Shows />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
-        <Login />
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
