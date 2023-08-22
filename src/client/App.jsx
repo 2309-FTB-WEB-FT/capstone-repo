4e3e330fc7fdb098fd2948fb9020e8323b87e891
RaccoonButler <raccoonutau@gmail.com>
2023-08-22 12:50:11 -0500

    Switch to search results page when searching

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React, { useState } from 'react';
+import { useRoutes, useNavigate } from 'react-router-dom'; // Import useRoutes and useNavigate
 import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Shows from './components/Shows';
 import Profile from './components/Profile';
 import SearchBar from './components/SearchBar';
-import SearchResults from './components/SearchResults'; // Import the SearchResults component
+import SearchResults from './components/SearchResults';
 
 function App() {
   const [searchResults, setSearchResults] = useState([]); // State for search results
@@ -20,23 +21,42 @@ function App() {
   };
 
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Manage login state
+  const navigate = useNavigate(); 
+
+  
+  const routing = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/Shows', element: <Shows /> },
+    {
+      path: '/Login',
+      element: <Login setIsLoggedIn={setIsLoggedIn} navigate={navigate} />,
+    },
+    {
+      path: '/Profile',
+      element: isLoggedIn ? <Profile /> : null,
+    },
+    {
+      path: '/SearchResults',
+      element: <SearchResults results={searchResults} standalone />,
+    },
+  ]);
 
   return (
     <BrowserRouter>
       <div className="App">
-                <SearchBar onSearch={handleSearch} /> {/* Add the SearchBar component */}
+        <SearchBar onSearch={handleSearch} /> {/* Add the SearchBar component */}
         <Navbar isLoggedIn={isLoggedIn} /> {/* Pass isLoggedIn to Navbar */}
         <h1>Boilerplate</h1>
-        <img id='comp-img' src='./computer.png' alt="computer" />
+        <img id="comp-img" src="./computer.png" alt="computer" />
         <p>Replace the starter code in this template with something cool</p>
 
-        {searchResults.length > 0 && <SearchResults results={searchResults} />} {/* Display search results */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Shows" element={<Shows />} />
-          <Route path="/Login" element={<Login setIsLoggedIn={setIsLoggedIn} />} /> {/* Pass setIsLoggedIn */}
-          {isLoggedIn && <Route path="/Profile" element={<Profile />} />} {/* Render Profile route only when logged in */}
-        </Routes>
+        {/* Display search results */}
+        {searchResults.length > 0 && (
+          <SearchResults results={searchResults} standalone />
+        )}
+
+        {/* Render the defined routes */}
+        {routing}
       </div>
     </BrowserRouter>
   );
--- src/client/components/SearchBar.jsx
+++ src/client/components/SearchBar.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './SearchBar.css';
 
 const SearchBar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
 
   const handleSearch = () => {
-    // Call the onSearch prop with the searchQuery
+    
     onSearch(searchQuery);
+
+    // Navigate to the search results page
+    navigate('/SearchResults');
   };
 
   return (
     <div className="search-bar-container">
       <input
-      className="search-input"
+        className="search-input"
         type="text"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="Search..."
       />
-      <button className="search-button" onClick={handleSearch}>Search</button>
+      <button className="search-button" onClick={handleSearch}>
+        Search
+      </button>
     </div>
   );
 };
