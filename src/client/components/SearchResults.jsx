d4864a046bc74732b78b0cc14e2577119c4faf7d
RaccoonButler <raccoonutau@gmail.com>
2023-08-22 12:41:46 -0500

    creating search results page

--- src/client/components/SearchResults.jsx
+++ src/client/components/SearchResults.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const SearchResults = ({ results }) => {
+const SearchResults = ({ results, standalone }) => {
+  if (standalone) {
+    return (
+      <div>
+        <h2>Search Results</h2>
+        {results.map((result) => (
+          <div key={result.id} className="search-result">
+            {/* Display the result content */}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="search-results">
       {results.map((result) => (
         <div key={result.id} className="search-result">
-
+          {/* Display the result content */}
         </div>
       ))}
     </div>
