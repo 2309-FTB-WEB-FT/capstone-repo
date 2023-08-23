2a28e09ddeafb02b6964d6017dd1c89dea3907bb
RaccoonButler <raccoonutau@gmail.com>
2023-08-23 02:40:51 -0500

    trying and struggling with the endpoints

---
+++ src/server/api/shows.js
@@ -0,0 +1,30 @@
+const express = require(`express`);
+const router = express.Router();
+const { getAllShows, getShowById, createShow, updateShow, deleteShow, getShowByID, getShowByTitle } = require('../db/shows');
+const { showData } = require(`../db/showData`)
+
+
+
+router.get('/', async (req, res, next) => {
+    try {
+       
+        const shows = await getAllShows();
+        console.log(shows)
+        res.send(shows);
+   } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/:name', async (req, res, next) => {
+    try {
+        console.log('hello')
+        const show = await getShowByTitle(req.params.name);
+        console.log(show.name);
+        res.send(show.name);
+   } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router
--- src/server/db/shows.js
+++ src/server/db/shows.js
@@ -1,8 +1,10 @@
-const db = require('./client')
+const db = require('./client');
+const { getShow, updateShow, deleteShow } = require('../db');
+//const router = express.Router();
 
 const createShow = async({ name, genre, image, description, averageLength }) => {
     try {
-        const { rows: [show ] } = await db.query(`
+        const { rows: [ show ] } = await db.query(`
         INSERT INTO shows(name, genre, image, description, averageLength)
         VALUES($1, $2, $3, $4, $5)
         RETURNING *`,  [name, genre, image, description, averageLength]);
@@ -13,4 +15,32 @@ const createShow = async({ name, genre, image, description, averageLength }) => 
     }
 }
 
-module.exports = {createShow}
+async function getAllShows(){
+    try {
+        console.log('hiiiiii')
+        const {rows} = await db.query(`
+            SELECT * FROM shows;
+        `);
+        //console.log(rows)
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function getShowByTitle(title) {
+    try {
+        console.log('yo')
+        const { rows } = await db.query(`
+            SELECT * FROM shows
+            WHERE name ILIKE $1
+        `, [`%${title}%`]);
+
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+module.exports = {createShow, getAllShows, getShowByTitle}
