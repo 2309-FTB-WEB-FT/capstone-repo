58e8c73afb8bfa5a5b6b4a026afcf4a4837bb315
Blake Holcombe <bwholcombe@gmail.com>
2023-08-24 13:02:20 -0500

    created the postReview function and the render new review function. Hope they work? lol

--- src/server/db/reviews.js
+++ src/server/db/reviews.js
@@ -13,6 +13,39 @@ const createReview = async({title, body, showName, userName, timestamp}) => {
     }
 }
 
+const postReview = async(review) => {
+    try {
+        const response = await fetch('/shows/show/:id/reviews', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(review),
+          });
+          const newReview = await response.json();
+          return newReview;
+    } catch (err) {
+        throw err;
+    }
+}
 
+function renderNewReviewForm() {
+    const newReviewForm = document.querySelector('#new-review-form');
+    newReviewForm.innerHTML = `
+      <form>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" />
+        <label for="body">Body</label>
+        <input type="text" name="body" id="body" />
+        <label for="showName">Show:</label>
+        <input type="text" name="showName" id="showName" />
+        <label for="userName">Review Author</label>
+        <input type="text" name="userNamee" id="userName" />
+        <label for="timestamp">timestamp</label>
+        <input type="date" name="timestamp" id="timestamp" />
+        <button type="submit">Submit</button>
+      </form>
+    `;
+  }
 
-module.exports = {createReview}
+module.exports = {createReview, postReview, renderNewReviewForm}
