998f44ca02609b653dbd9701af882d142dc7d1b8
Blake Holcombe <bwholcombe@gmail.com>
2023-08-31 09:13:40 -0500

    changes I made yesterday

--- src/server/api/reviews.js
+++ src/server/api/reviews.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const reviewRouter = express.Router();
 const { getAllReviews } = require('../db/reviews')
+const { getUserById } = require('../db/users')
 
 reviewRouter.get('/', async( req, res, next) => {
   
@@ -16,7 +17,29 @@ reviewRouter.get('/', async( req, res, next) => {
     }
 });
 
-reviewRouter.post
+reviewRouter.post('/', async( req, res, next) => {
+      const { title, body } = req.body
+    try {
+        const parsedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await getUserById(parsedToken.id);
+    
+         if(user) {
+                const token = jwt.sign({
+                    id: user.id,
+                    email: user.email
+                }, process.env.JWT_SECRET, {
+                    expiresIn: '1w'
+                });
+    
+                res.send({
+                
+                });}
+
+        
+    } catch(err) {
+        next(err)
+    }
+});
 
 reviewRouter.delete
 
