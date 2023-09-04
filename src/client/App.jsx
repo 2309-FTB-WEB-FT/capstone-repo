4b5da27d755a7edbbed9c6dc2640da529dc45707
RaccoonButler <raccoonutau@gmail.com>
2023-09-04 16:39:07 -0500

    tryinnggg something

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -25,7 +25,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Login" element={<Login setIsLoggedIn={setIsLoggedIn} setToken={setToken} />} />
           <Route path="/Logout" element={<Logout setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="/UserProfile" element={<UserProfile token={token} username={username} />} />
+          <Route path="/UserProfile" element={<UserProfile token={token} />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -1,78 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
-const UserProfile = ({ token, username }) => {
-  const { username: paramUsername } = useParams();
-  const [userData, setUserData] = useState({
-    name: '',
-    email: '',
-    profilePhoto: 'https://shorturl.at/dxzM3',
-    likedShows: [],
-    pastReviews: [],
-  });
+const Navbar = ({ isLoggedIn, token, username }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        // Use the username from the URL parameter or the one passed as a prop
-        const targetUsername = paramUsername || username;
-
-        const response = await fetch(`http://localhost:3000/api/users/${targetUsername}`, {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (response.ok) {
-          const result = await response.json();
-          setUserData({
-            ...result,
-            likedShows: result.likedShows || [],
-            pastReviews: result.pastReviews || [],
-          });
-        } else {
-          // Handle API error
-          console.error('API error');
-        }
-      } catch (error) {
-        console.error('API request error:', error);
-      }
-    };
-
-    fetchUserData();
-  }, [token, username, paramUsername]);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className="user-profile">
-      <div className="profile-header">
-        <div className="profile-photo">
-          <img src="https://shorturl.at/dxzM3" alt="Profile" />
-        </div>
-        <div className="profile-info">
-          <h2>{userData.name}</h2>
-        </div>
+    <div className={`navbar ${isOpen ? 'open' : ''}`}>
+      <div className="toggle-button" onClick={toggleSidebar}>
+        ☰
       </div>
-      <div className="liked-shows">
-        <h3>Liked Shows</h3>
-        {userData.likedShows.map((show) => (
-          <div key={show.id}>
-            <p>{show.title}</p>
-            <img src={show.image} alt={show.title} />
-          </div>
-        ))}
-      </div>
-      <div className="past-reviews">
-        <h3>Past Reviews</h3>
-        {userData.pastReviews.map((review) => (
-          <div key={review.id}>
-            <p>Review for: {review.showTitle}</p>
-            <p>{review.content}</p>
-          </div>
-        ))}
+      <div className="sidebar">
+        <div className="logo">
+          <img src="/bingeit.png" alt="logo" />
+        </div>
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          {isLoggedIn ? (
+            <>
+              {/* Link to the UserProfile using the username */}
+              <li><Link to={`/UserProfile/${username}`}>Profile</Link></li>
+              <li><Link to="/logout">Logout</Link></li>
+            </>
+          ) : (
+            <li><Link to="/Login">Login</Link></li>
+          )}
+        </ul>
       </div>
     </div>
   );
 };
 
-export default UserProfile;
+export default Navbar;
