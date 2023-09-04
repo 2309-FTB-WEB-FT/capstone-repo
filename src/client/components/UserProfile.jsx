f088245c4c26288d60be96a881466c35c3fa458d
RaccoonButler <raccoonutau@gmail.com>
2023-09-04 16:47:28 -0500

    not working rn

--- src/client/components/UserProfile.jsx
+++ src/client/components/UserProfile.jsx
@@ -17,14 +17,14 @@ const UserProfile = ({ token }) => {
       try {
         // Use the username from the URL parameter or the one fetched from the server
         const targetUsername = paramUsername || username;
-
+    
         const response = await fetch(`http://localhost:3000/api/users/${targetUsername}`, {
           method: 'GET',
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-
+    
         if (response.ok) {
           const result = await response.json();
           setUserData({
@@ -34,12 +34,12 @@ const UserProfile = ({ token }) => {
           });
         } else {
           // Handle API error
-          console.error('API error');
+          console.error('API error:', response.status, response.statusText);
         }
       } catch (error) {
         console.error('API request error:', error);
       }
-    };
+    };    
 
     fetchUserData();
   }, [token, username, paramUsername]);
@@ -74,7 +74,32 @@ const UserProfile = ({ token }) => {
 
   return (
     <div className="user-profile">
-      {/* ... */}
+      <div className="profile-header">
+        <div className="profile-photo">
+          <img src="https://shorturl.at/dxzM3" alt="Profile" />
+        </div>
+        <div className="profile-info">
+          <h2>{userData.name}</h2>
+        </div>
+      </div>
+      <div className="liked-shows">
+        <h3>Liked Shows</h3>
+        {userData.likedShows.map((show) => (
+          <div key={show.id}>
+            <p>{show.title}</p>
+            <img src={show.image} alt={show.title} />
+          </div>
+        ))}
+      </div>
+      <div className="past-reviews">
+        <h3>Past Reviews</h3>
+        {userData.pastReviews.map((review) => (
+          <div key={review.id}>
+            <p>Review for: {review.showTitle}</p>
+            <p>{review.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
